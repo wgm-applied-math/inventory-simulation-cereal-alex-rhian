871fa7ee1ce9caf7a6b7dee5fedb6b613bdc032b
AlexBaham1999 <bahama@g.cofc.edu>
2024-04-22 17:21:18 -0400

    mean values
    mean values

--- run_Inventory.m
+++ run_Inventory.m
@@ -73,6 +73,7 @@ TotalOrders(1, j) = numbacklog(1, j) + length(inventory.Fulfilled);
 Fraction_of_Orders(1, j) = TotalBacklog(1, j)/TotalOrders(1, j);
 end
 
+%The mean value for fraction of orders is .0237.
 
 fig1 = figure();
 t1 = tiledlayout(fig1, 1, 1);
@@ -97,6 +98,8 @@ for j = 1:NumSamples
     end
     Fraction_of_Days(1, j) = daysbacklogged(1, j)/10;
 end
+
+%The mean value for fraction of days is .1810.
 
 fig2 = figure();
 t2 = tiledlayout(fig2, 1, 1);
@@ -127,6 +130,8 @@ end
 DelayTimeVector = reshape(DelayTime, 1, NumSamples*maxFulfilled);
 DelayTimeVectorNoZeros = DelayTimeVector(DelayTimeVector ~= 0);
 
+%The mean value delay time is .3479.
+
 fig3 = figure();
 t3 = tiledlayout(fig3, 1, 1);
 ax3 = nexttile(t3);
@@ -153,6 +158,8 @@ end
 TotalBacklogVector = reshape(Total_Backlog_Amount, 1, NumSamples*MaxTime);
 TotalBacklogVectorNoZeros = TotalBacklogVector(TotalBacklogVector ~= 0);
 
+%The mean value for total backlog amount is 46.0879.
+
 fig4 = figure();
 t4 = tiledlayout(fig4, 1, 1);
 ax4 = nexttile(t4);
@@ -175,6 +182,8 @@ TotalCosts = cellfun(@(i) i.RunningCost, InventorySamples);
 % that doesn't depend directly on how many time steps the samples run for.
 meanDailyCost = mean(TotalCosts/MaxTime);
 fprintf("Mean daily cost: %f\n", meanDailyCost);
+
+%The mean value for daily cost is $240.1188.
 
 %% Make pictures
 
